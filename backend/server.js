40b9c046e245738c32f84b06251bacaf13dd2c6c
Lawrence <locaulker@gmail.com>
2021-02-15 01:15:56 -0500

    implemented serving products from express server

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+// using commonjs module instead of es module
+const express = require("express")
+const products = require("./data/products")
+
+// initialize express
+const app = express()
+
+// routes
+app.get("/", (req, res) => {
+  res.send("API is running...")
+})
+
+app.get("/api/products", (req, res) => {
+  res.json(products)
+})
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
+
+// server port
+app.listen(5000, console.log("Server running on port 5000"))
