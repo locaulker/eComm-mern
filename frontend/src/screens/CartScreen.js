57f53a73d851f6021f96c2de8965edf55f9a5928
Lawrence <locaulker@gmail.com>
2021-02-18 07:03:35 -0500

    resume at 6-32 @3:31

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,13 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { Row, Col, ListGroup, Image, Form, Button, Card } from "react-bootstrap"
+import Message from "../components/Message"
+import { addToCart } from "../actions/cartActions"
 
-const CartScreen = () => {
+const CartScreen = ({ match, location, history }) => {
+  const productId = match.params.id
+
   return <div>Cart</div>
 }
 
