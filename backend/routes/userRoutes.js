e7dba247b43a4179d983ced4ac77c4f23b057bed
Lawrence O. Caulker <locaulker@gmail.com>
2021-02-19 23:08:24 -0500

    completed backend user authemtication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,14 @@
 import express from "express"
 const router = express.Router()
 
-import { authUser, getUserProfile } from "../controllers/userController.js"
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js"
 import { protect } from "../middleware/authMiddleware.js"
 
-/**
- * @desc  login to users
- * @route POST /login
- * @access Public
- */
+router.route("/").post(registerUser)
 router.post("/login", authUser)
 router.route("/profile").get(protect, getUserProfile)
 
